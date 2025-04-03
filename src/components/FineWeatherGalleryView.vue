7bbdd28927cd4d35750b7bb38d9ab3ee4ec8a282
cjieliang <cjieliang980515@qq.com>
2025-04-03 22:14:36 +0800

    test

--- src/components/FineWeatherGalleryView.vue
+++ src/components/FineWeatherGalleryView.vue
@@ -128,8 +128,8 @@ import ImageDetail from './ImageDetail.vue';
 import imagesEntireRaw from '../assets/images.json';
 
 const PAGE_SIZE = 20;
-const TITLE = '「一些晴朗的日子」';
-const INTRO = `由于有若干个能拍照的设备，再加上时间会公平地杀死一切，我每年都会拍出几张回看时感慨万千的照片。但由于时间与地域的关系，这些照片往往要么丢失，要么被随意塞在网盘的某处。于是，现在（2022-11-28）我决心花一些精力把它们维护起来。然而，正如前面所言，原图已不易寻觅，目前所得的一些图片大多来自微信朋友圈或微博，很遗憾图片质量已损失太多。而这些图片拍时多是好天气，所以干脆统称这些照片为${TITLE}。`;
+const TITLE = '「回忆」';
+const INTRO = `记录着我们那些开心的日子`;
 const emojiReactionKey = 'fine-weather.tkzt.cn';
 const apiBase = import.meta.env.VITE_API_BASE;
 const emojiReactions = ref([]);
